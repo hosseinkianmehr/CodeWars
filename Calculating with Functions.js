6e18adce5ad477dd2229ce1939de94c59acc8cae
Hossein <68367971+hosseinkianmehr@users.noreply.github.com>
2022-11-05 17:20:26 +0330

    Calculating with Functions L5
    Calculating with Functions  L5
    https://www.codewars.com/kata/525f3eda17c7cd9f9e000b39/solutions/javascript

---
+++ Calculating with Functions.js
@@ -0,0 +1,150 @@
+function zero(props) {if(props){
+  switch (props[0]) {
+  case '-':
+    return parseInt(0 - props[1]);
+    break;
+  case '+':
+    return parseInt(0 + props[1]);
+    break;
+  case '*':
+     return parseInt(0 * props[1]);
+    break;
+  case '/':
+    return parseInt(0 / props[1]);
+    break;
+}
+}else{return 00}}
+function one(props) {if(props){switch (props[0]) {
+  case '-':
+    return parseInt(1 - props[1]);
+    break;
+  case '+':
+    return parseInt(1 + props[1]);
+    break;
+  case '*':
+     return parseInt(1 * props[1]);
+    break;
+  case '/':
+    return parseInt(1 / props[1]);
+    break;
+}}else{return 1}}
+function two(props) {if(props){switch (props[0]) {
+  case '-':
+    return parseInt(2 - props[1]);
+    break;
+  case '+':
+    return parseInt(2 + props[1]);
+    break;
+  case '*':
+     return parseInt(2 * props[1]);
+    break;
+  case '/':
+    return parseInt(2 / props[1]);
+    break;
+}}else{return 2}}
+function three(props) {if(props){switch (props[0]) {
+  case '-':
+    return parseInt(3 - props[1]);
+    break;
+  case '+':
+    return parseInt(3 + props[1]);
+    break;
+  case '*':
+     return parseInt(3 * props[1]);
+    break;
+  case '/':
+    return parseInt(3 / props[1]);
+    break;
+}}else{return 3}}
+function four(props) {if(props){switch (props[0]) {
+  case '-':
+    return parseInt(4 - props[1]);
+    break;
+  case '+':
+    return parseInt(4 + props[1]);
+    break;
+  case '*':
+     return parseInt(4 * props[1]);
+    break;
+  case '/':
+    return parseInt(4 / props[1]);
+    break;
+}}else{return 4}}
+function five(props) {if(props){switch (props[0]) {
+  case '-':
+    return parseInt(5 - props[1]);
+    break;
+  case '+':
+    return parseInt(5 + props[1]);
+    break;
+  case '*':
+     return parseInt(5 * props[1]);
+    break;
+  case '/':
+    return parseInt(5 / props[1]);
+    break;
+}}else{return 5}}
+function six(props) {if(props){switch (props[0]) {
+  case '-':
+    return parseInt(6 - props[1]);
+    break;
+  case '+':
+    return parseInt(6 + props[1]);
+    break;
+  case '*':
+     return parseInt(6 * props[1]);
+    break;7
+  case '/':
+    return parseInt(6 / props[1]);
+    break;
+}}else{return 6}}
+function seven(props) {if(props){switch (props[0]) {
+  case '-':
+    return parseInt(7 - props[1]);
+    break;
+  case '+':
+    return parseInt(7 + props[1]);
+    break;
+  case '*':
+     return parseInt(7 * props[1]);
+    break;
+  case '/':8
+    return parseInt(7 / props[1]);
+    break;
+}}else{return 7}}
+function eight(props) {if(props){switch (props[0]) {
+  case '-':
+    return parseInt(8 - props[1]);
+    break;
+  case '+':
+    return parseInt(8 + props[1]);
+    break;
+  case '*':9
+     return parseInt(8 * props[1]);
+    break;
+  case '/':
+    return parseInt(8 / props[1]);
+    break;
+}}else{return 8}}
+function nine(props) {if(props){switch (props[0]) {
+  case '-':
+    return parseInt(9 - props[1]);
+    break;
+  case '+':
+    return parseInt(9 + props[1]);
+    break;
+  case '*':
+     return parseInt(9 * props[1]);
+    break;
+  case '/':
+    return parseInt(9 / props[1]);
+    break;
+}}else{return 9}}
+
+function plus(props) {return ['+' , props]}
+function minus(props) {return [ '-' ,props]}
+function times(props) {return [ '*' ,props]}
+function dividedBy(props) {return [ '/' ,props]}
+
+
+
